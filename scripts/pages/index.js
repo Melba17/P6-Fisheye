aa7040e23be9234337b33b10670de20786a712b1
Melba17 <evabancerowski77@gmail.com>
2024-07-08 18:04:55 +0200

    debut du projet Fisheye

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,46 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+import { photographerTemplate } from "../templates/photographer.js";
+
+async function getPhotographers() {
+    try {
+        // Chemin vers votre fichier JSON 
+        const response = await fetch("../../data/photographers.json");
+        
+        // Vérifiez que la réponse est correcte
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        // Désérialisation du fichier JSON - réponse en tableau
+        const data = await response.json();
+        console.log('Données récupérées :', data);  // Debugging
+
+        // Retourne le tableau des photographes
+        return data.photographers;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des photographes :', error);
     }
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+async function displayData(photographers) {
+    // Récupération de la section où l'on veut insérer les vignettes
+    const photographersSection = document.querySelector(".photographer_section");
+    // On parcours le tableau pour ajouter le squelette modèle de présentation à chaque photographe
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        // Ajout des vignette à la section html DOM
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+async function init() {
+    // Récupère les datas des photographes..
+    const photographers = await getPhotographers();
+    if (photographers) {
+        //.. et l'affiche
         displayData(photographers);
     }
-    
-    init();
-    
+}
+// Activation de la fonction
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,57 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+////////// SQUELETTE DE CHAQUE VIGNETTE ///////////////
+export function photographerTemplate(data) {
+    // Nom du photographe et nom de l'image = données JSON
+    const { id,name, portrait, city, country, tagline, price } = data;
+    // Chemin vers l'image selon son Nom dans dossier img
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+    console.log('Chemin de l\'image :', picture); // Debugging
 
     function getUserCardDOM() {
+        // Création de la vignette avec la balise <article>...
         const article = document.createElement( 'article' );
+
+
+        // Création du lien qui entoure et encapsule l'img et le h2
+        const link = document.createElement('a');
+        link.setAttribute("href", `photographer.html?id=${id}`);
+        // Rendre focusable via la navigation au clavier
+        // L'attribut "tabindex" est utilisé pour contrôler l'ordre de tabulation des éléments focusables dans la page web. L'ordre de tabulation est la séquence dans laquelle les éléments reçoivent le focus lorsque l'utilisateur navigue dans la page en utilisant la touche Tab. "0" c'est à dire focusable dans l'ordre naturel du document donc inclus dans l'ordre de tabulation par défaut de la page => améliore l'accessibilité et l'interactivité du site
+        link.setAttribute("tabindex", "0"); 
+
+        // Création de l'image
         const img = document.createElement( 'img' );
+        // .. ajout à l'img d'un attribut src contenant le nom de l'image
         img.setAttribute("src", picture)
+        // l'attribut alt de l'image est défini avec le nom du photographe pour améliorer l'accessibilité
+        img.setAttribute("alt", name); 
+        // Création du titre h2
         const h2 = document.createElement( 'h2' );
+        // .. qui prend le nom du photographe en valeur
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        // Ajout de l'image et du titre h2 dans le lien
+        link.appendChild(img);
+        link.appendChild(h2);
+
+        // Ajout des autres éléments
+        const location = document.createElement( 'h3' );
+        location.textContent = `${city}, ${country}`;
+        
+        const p = document.createElement( 'strong' );
+        p.textContent = tagline;
+        
+        const span = document.createElement( 'span' );
+        span.textContent = `${price}€/jour`;
+
+        // Ajout à la balise <article> de l'ensemble des infos
+        article.appendChild(link);
+        article.appendChild(location);
+        article.appendChild(p);
+        article.appendChild(span);
+        
+        // Retourne l'ensemble du contenu de la vignette
         return (article);
     }
+    // Affiche le tout sur la page web lorsqu'appelé
     return { name, picture, getUserCardDOM }
 }
