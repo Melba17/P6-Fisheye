2cb865dea2eae9df4ceed7d10bc16d6d56ac7887
Melba17 <evabancerowski77@gmail.com>
2024-07-09 17:33:31 +0200

    suite page accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,35 @@
+// Instruction qui importe la fonction "photographerTemplate" à partir du module/fichier "photographer.js" pour pouvoir l'utiliser ici
 import { photographerTemplate } from "../templates/photographer.js";
 
+// RECUPERATION DES DONNEES JSON 
+// opération asynchrone avec "async" et "await" pour traiter la réponse du serveur: c'est à dire permet au navigateur d'afficher normalement les infos à l'écran en attendant la réponse du serveur
 async function getPhotographers() {
     try {
-        // Chemin vers votre fichier JSON 
+        // Requête "fetch" contenant le chemin vers fichier JSON (chemin relatif) pour récupérer les données du fichier
         const response = await fetch("../../data/photographers.json");
         
-        // Vérifiez que la réponse est correcte
+        // Vérifie que la réponse est correcte sinon on signale un problème
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        
-        // Désérialisation du fichier JSON - réponse en tableau
+        // Stockage de la réponse du serveur dans la variable "data" avec désérialisation du fichier JSON - ici réponse sous forme d'objet lisible par le navigateur
         const data = await response.json();
-        console.log('Données récupérées :', data);  // Debugging
-
+         // Debugging
+        console.log('Données récupérées :', data); 
         // Retourne le tableau des photographes
         return data.photographers;
     } catch (error) {
         console.error('Erreur lors de la récupération des photographes :', error);
     }
 }
+// "try/catch" ou "throw new error" pour centraliser et gérer nous-même les erreurs si il y en a. Donc la console ne pourra plus afficher "uncaught".
 
 
+/////// INSERTION DES ELEMENTS VIGNETTES DANS LE DOM ///////////
 async function displayData(photographers) {
     // Récupération de la section où l'on veut insérer les vignettes
     const photographersSection = document.querySelector(".photographer_section");
-    // On parcours le tableau pour ajouter le squelette modèle de présentation à chaque photographe
+    // On parcours le tableau pour ajouter le squelette des vignettes représentant chaque photographe / Méthode "forEach" qui itère la fonction fléchée (photographer)=>{} sur chaque objet(photographes) du tableau
     photographers.forEach((photographer) => {
         const photographerModel = photographerTemplate(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
@@ -34,6 +38,7 @@ async function displayData(photographers) {
     });
 }
 
+//// AFFICHAGE FINAL DES VIGNETTES SUR LA PAGE WEB /////
 async function init() {
     // Récupère les datas des photographes..
     const photographers = await getPhotographers();
@@ -42,5 +47,5 @@ async function init() {
         displayData(photographers);
     }
 }
-// Activation de la fonction
-init();
+// Ici, un écouteur d'évènement qui permet l'activation de la fonction finale lorsque le DOM est complètement chargé
+document.addEventListener('DOMContentLoaded', init);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,18 +1,19 @@
-////////// SQUELETTE DE CHAQUE VIGNETTE ///////////////
+////////// SQUELETTE DES VIGNETTES ///////////////
+// L'objet retourné par photographerTemplate(data) permet à d'autres parties du code d'accéder aux données du photographe ainsi qu'à la méthode pour créer la vignette, ce qui permet une meilleure modularité et réutilisabilité.
 export function photographerTemplate(data) {
-    // Nom du photographe et nom de l'image = données JSON
+    // Infos diverses photographes = données JSON
     const { id,name, portrait, city, country, tagline, price } = data;
-    // Chemin vers l'image selon son Nom dans dossier img
+    // Chemin vers l'image dans dossier assets selon le nom du photographe en question
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
-    console.log('Chemin de l\'image :', picture); // Debugging
 
     function getUserCardDOM() {
-        // Création de la vignette avec la balise <article>...
+        // Création de la balise <article> pour L'emplacement de la vignette...
         const article = document.createElement( 'article' );
 
 
-        // Création du lien qui entoure et encapsule l'img et le h2
+        // Création du lien avec la balises <a> qui entoure et encapsule l'img et le h2
         const link = document.createElement('a');
+        // Lien vers la page de chaque photographe selon leur Id respectif / ?id=${id} étant le paramètre voulu dans l'url
         link.setAttribute("href", `photographer.html?id=${id}`);
         // Rendre focusable via la navigation au clavier
         // L'attribut "tabindex" est utilisé pour contrôler l'ordre de tabulation des éléments focusables dans la page web. L'ordre de tabulation est la séquence dans laquelle les éléments reçoivent le focus lorsque l'utilisateur navigue dans la page en utilisant la touche Tab. "0" c'est à dire focusable dans l'ordre naturel du document donc inclus dans l'ordre de tabulation par défaut de la page => améliore l'accessibilité et l'interactivité du site
@@ -29,29 +30,36 @@ export function photographerTemplate(data) {
         // .. qui prend le nom du photographe en valeur
         h2.textContent = name;
 
-        // Ajout de l'image et du titre h2 dans le lien
+        // Ajout de l'image et du titre h2 dans le lien/balise <a>
         link.appendChild(img);
         link.appendChild(h2);
 
-        // Ajout des autres éléments
+        ///// Ajout des autres éléments /////
         const location = document.createElement( 'h3' );
+        // Interpolation
         location.textContent = `${city}, ${country}`;
         
+        // Mise en évidence avec <strong>
         const p = document.createElement( 'strong' );
         p.textContent = tagline;
         
         const span = document.createElement( 'span' );
         span.textContent = `${price}€/jour`;
 
-        // Ajout à la balise <article> de l'ensemble des infos
+        // Ajout de l'ensemble des infos à la balise <article> 
         article.appendChild(link);
         article.appendChild(location);
         article.appendChild(p);
         article.appendChild(span);
-        
-        // Retourne l'ensemble du contenu de la vignette
+
+        // Affiche l'article dans la console
+        console.log(article); 
+        // Retourne l'élément DOM <article> (ensemble du contenu de la vignette ), qui peut être directement utilisé pour afficher le contenu sur la page web.
         return (article);
+        
     }
-    // Affiche le tout sur la page web lorsqu'appelé
-    return { name, picture, getUserCardDOM }
+    // Retourne un objet qui contient des données sur le photographe et une méthode pour créer un élément DOM. Cet objet peut être utilisé pour gérer et manipuler les informations et le rendu des vignettes des photographes de manière plus flexible et structurée.
+    const result = { name, picture, getUserCardDOM };
+    console.log(result); 
+    return result;
 }
