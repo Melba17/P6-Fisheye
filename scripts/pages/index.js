2bb446efc380050530981c52bb26c6ba2f7d672f
Melba17 <evabancerowski77@gmail.com>
2024-07-26 13:48:55 +0200

    retravaille de la modale et détails divers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -16,23 +16,29 @@ async function getPhotographers() {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+        
         // Stockage de la réponse du serveur dans la variable "data" avec désérialisation du fichier JSON - ici réponse sous forme d'objet lisible par le navigateur
         const data = await response.json();
-         // Debugging
-        console.log('Données récupérées :', data); 
+        
+        // Debugging : affichage des données récupérées dans la console
+        console.log('Données récupérées :', data);
+        
         // Retourne le tableau des photographes
         return data.photographers;
     } catch (error) {
+        // Affichage d'une erreur en cas de problème lors de la récupération des données
         console.error('Erreur lors de la récupération des photographes :', error);
     }
 }
+
 // "try/catch" ou "throw new error" pour centraliser et gérer nous-même les erreurs si il y en a. Donc la console ne pourra plus afficher "uncaught".
 
 /////////////////// 2 FONCTIONS INTERDEPENDANTES : LA 1ERE EXTRAT l'ID DU PHOTOGRAPHE À PARTIR DE L'URL - LA 2EME UTILISE CET ID POUR AFFICHER LES DETAILS DU PHOTOGRAPHE DANS SA BANNIERE ////////////////////
-// Fonction pour récupérer l'ID du photographe à partir de l'URL/page web où l'on se trouve
+// Fonction pour récupérer l'ID du photographe à partir de l'URL
 function getPhotographerIdFromUrl() {
     // Création de l'objet URL / "new URL" crée un nouvel objet URL basé sur cette URL complète / "window.location.href" récupère l'URL complète actuellement affichée dans la barre d'adresse du navigateur = en facilite la manipulation
     const url = new URL(window.location.href);
+    
     // Récupère et retourne l'id du photographe en question situé à la fin de l'url (ex : ?id=243)
     return url.searchParams.get('id');
 }
@@ -41,41 +47,51 @@ function getPhotographerIdFromUrl() {
 async function displayPhotographerDetails(photographerId) {
     // Récupération des données JSON globales contenant la liste des photographes / fonction getPhotographers() ci-dessus
     const photographers = await getPhotographers();
+    
     // Recherche du photographe correspondant à l'ID fourni donc parcourt la liste des photographes et retourne le photographe dont l'ID correspond à photographerId
     const photographer = photographers.find(p => p.id == photographerId);
 
-    // // Vérification si le photographe a été trouvé
+    // Vérification si le photographe a été trouvé
     if (photographer) {
         // Génération du modèle de photographe en utilisant la fonction template du fichier "photographer.js" dans le dossier "templates"
         const photographerModel = photographerTemplate(photographer);
+        
         // Extraction des éléments spécifiques du modèle du photographe
         const { h2, h3, strong, img } = photographerModel.getSpecificElements();
 
         // Sélection de la section d'information sur le photographe dans le DOM
         const infoSection = document.querySelector(".photograph-info");
+        
         // Ajout des éléments d'information (titre, sous-titre, etc.) à la section d'information
         infoSection.appendChild(h2);
         infoSection.appendChild(h3);
         infoSection.appendChild(strong);
+        
         // Sélection de la section de l'image du photographe dans le DOM
         const imageSection = document.querySelector(".photograph-image");
+        
         // Ajout de l'image du photographe à la section de l'image
         imageSection.appendChild(img);
 
+        // Création de la modale de contact
         createModal();
 
+        // Ajout du nom du photographe dans l'encart prévu de la modale
         const nameElement = document.querySelector(".photographer-name");
         nameElement.textContent = photographer.name; 
 
+        // Gestion de l'ouverture et de la fermeture de la modale
         document.querySelector(".contact_button").addEventListener("click", openModal);
         document.querySelector(".modal_close").addEventListener("click", closeModal);
+        
+        
+        // Validation du formulaire lors de la soumission
         document.querySelector("form").addEventListener("submit", function(event) {
             if (validateForm()) {
-                
-                console.log("Le formulaire est envoyé.");
-                closeModal();
+                console.log("Le formulaire est envoyé."); // Affichage d'un message de succès
+                closeModal(); // Fermeture de la modale après soumission réussie
             } else {
-                event.preventDefault();
+                event.preventDefault(); // Empêche la soumission du formulaire en cas d'erreur
             }
         });
 
@@ -85,36 +101,42 @@ async function displayPhotographerDetails(photographerId) {
     }
 }
 
-
 //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
 
 /////// INSERTION DES ELEMENTS VIGNETTES DANS LE DOM POUR LA PAGE D'ACCUEIL ///////////
+// Fonction pour insérer les vignettes des photographes dans le DOM pour la page d'accueil
 async function displayData(photographers) {
     // Récupération de la section où l'on veut insérer les vignettes
     const photographersSection = document.querySelector(".photographer_section");
+    
     // On parcours le tableau pour ajouter le squelette des vignettes représentant chaque photographe / Méthode "forEach" qui itère la fonction fléchée (photographer)=>{} sur chaque objet(photographes) du tableau
     photographers.forEach((photographer) => {
+        // Génération du modèle de photographe à l'aide de la fonction template
         const photographerModel = photographerTemplate(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
-        // Ajout des vignette à la section html DOM
+        
+        // Ajout des vignettes à la section HTML DOM
         photographersSection.appendChild(userCardDOM);
     });
 }
 
-
-
 //// AFFICHAGES FINAUX /////
 async function init() {
+    // Récupération de l'ID du photographe depuis l'URL
     const photographerId = getPhotographerIdFromUrl();
+    
+    // Si un ID est présent, afficher les détails du photographe
     if (photographerId) {
         await displayPhotographerDetails(photographerId);
     } else {
+        // Sinon, récupérer et afficher les données des photographes
         const photographers = await getPhotographers();
         if (photographers) {
-            displayData(photographers);
+            displayData(photographers); // Affichage des vignettes des photographes
         }
     }
 }
+
 // Activation/Appel de la fonction
-init()
+init();
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,81 +1,225 @@
 export function createModal() {
+    // Crée un conteneur pour la modale
     const modalContainer = document.createElement('div');
+    
+    // Assigne un identifiant unique pour le conteneur de la modale
     modalContainer.id = "contact_modal";
+    
+    // Définit le rôle de la modale pour les technologies d'assistance
     modalContainer.role = "dialog";
+    
+    // Ajoute une classe CSS pour le style de fond de la modale
     modalContainer.className = "bckg_modal";
+    
+    // Décrit le titre de la modale pour les technologies d'assistance
     modalContainer.setAttribute('aria-labelledby', "Contactez-moi");
-    modalContainer.style.display = "none"; // Modale cachée au départ
+    
+    // Cache la modale par défaut
+    modalContainer.style.display = "none";
 
+    // Définit le contenu HTML de la modale
     modalContainer.innerHTML = `
-        <div class="modal">
-            <header>
-                <div class="modal-header-content">
-                    <h2 id="Contactez-moi">Contactez-moi</h2>
-                    <div class="photographer-name"></div> 
-                </div>
-                <span class="modal_close" aria-label="bouton de fermeture"></span>
-            </header>
-            <form name="contactForm" action="" method="get" novalidate>
-                <div>
-                    <label for="first">Prénom</label>
-                    <input class="text-control" type="text" id="first" name="first" aria-labelledby="first" />
-                    <span class="error-message"></span>
-                </div>
-                <div>
-                    <label for="last">Nom</label>
-                    <input class="text-control" type="text" id="last" name="last" aria-labelledby="last" />
-                    <span class="error-message"></span>
-                </div>
-                <div>
-                    <label for="email">E-mail</label>
-                    <input class="text-control" type="email" id="email" name="email" aria-labelledby="email" />
-                    <span class="error-message"></span>
-                </div>
-                <div>
-                    <label for="message">Votre message</label>
-                    <textarea name="message" id="message" class="text-control" aria-labelledby="message"></textarea>
-                    <span class="error-message"></span>
-                </div>
-                <button class="submit_button" type="submit" aria-label="envoyer">Envoyer</button>
-            </form>
-        </div>
-    `;
+    <div class="modal">
+        <header>
+            <div class="modal-header-content">
+                <h2 id="Contactez-moi">Contactez-moi</h2>
+                <div class="photographer-name"></div> 
+            </div>
+            <button class="modal_close" aria-label="bouton de fermeture" type="button"></button>
+        </header>
+        <form name="contactForm" action="" method="get" novalidate>
+            <div>
+                <label for="first" id="label-first">Prénom</label>
+                <input class="text-control" type="text" id="first" name="first" aria-labelledby="label-first" />
+                <span class="error-message"></span>
+            </div>
+            <div>
+                <label for="last" id="label-last">Nom</label>
+                <input class="text-control" type="text" id="last" name="last" aria-labelledby="label-last" />
+                <span class="error-message"></span>
+            </div>
+            <div>
+                <label for="email" id="label-email">E-mail</label>
+                <input class="text-control" type="email" id="email" name="email" aria-labelledby="label-email" />
+                <span class="error-message"></span>
+            </div>
+            <div>
+                <label for="message" id="label-message">Votre message</label>
+                <textarea name="message" id="message" class="text-control" aria-labelledby="label-message"></textarea>
+                <span class="error-message"></span>
+            </div>
+            <button class="submit_button" type="submit" aria-label="envoyer">Envoyer</button>
+        </form>
+    </div>
+`;
 
+    // Ajoute le conteneur de la modale au body du document
     document.body.appendChild(modalContainer);
 
-    // Ajout des gestionnaires d'événements pour la validation et le logging
+    // Ajoute des gestionnaires d'événements aux champs du formulaire pour la validation et le logging
     const fields = ['first', 'last', 'email', 'message'];
     fields.forEach(fieldId => {
         const field = document.getElementById(fieldId);
         if (field) {
+            // Ajoute un écouteur d'événement pour la perte de focus (blur)
             field.addEventListener('blur', () => {
-                validateField(fieldId);
-                logFieldValue(fieldId);
+                validateField(fieldId); // Valide le champ
+                logFieldValue(fieldId); // Logge la valeur du champ
             });
         }
     });
-    
-    // Gestionnaire pour la validation du formulaire
+
+    // Ajoute un gestionnaire d'événements pour la soumission du formulaire
     document.querySelector("form").addEventListener("submit", function(event) {
         if (!validateForm()) {
-            event.preventDefault(); // Empêche le navigateur de gérer la soumission si les validations échouent
+            // Si le formulaire n'est pas valide, empêche la soumission
+            event.preventDefault();
+        } else {
+            // Si le formulaire est valide
+            event.preventDefault(); // Empêche la soumission réelle
+            const photographerId = new URLSearchParams(window.location.search).get('id');
+            if (photographerId) {
+                // Redirige vers la page du photographe avec l'ID dans l'URL
+                window.location.href = `photographer.html?id=${photographerId}`;
+                // Réinitialise le formulaire après un petit délai pour permettre la redirection
+                setTimeout(() => {
+                    resetForm(); // Réinitialise les champs du formulaire
+                }, 100);
+            }
         }
     });
+
+    // Assure que le bouton de fermeture a un gestionnaire d'événement pour fermer la modale
+    const closeButton = document.querySelector('.modal_close');
+    if (closeButton) {
+        closeButton.addEventListener('click', closeModal);
+    }
 }
 
 export function openModal() {
-    toggleModal(true);
+    toggleModal(true); // Affiche la modale
+
+    // Utilise setTimeout pour garantir que le focus est appliqué après que la modale soit complètement affichée
+    setTimeout(() => {
+        const firstFocusableElement = document.querySelector('#contact_modal .text-control');
+        if (firstFocusableElement) {
+            // Met le focus sur le premier champ de formulaire de la modale
+            firstFocusableElement.focus();
+        }
+    }, 0); // Le délai de 0 assure que le code s'exécute juste après la visibilité de la modale
+
+    // Ajoute un gestionnaire pour la navigation avec les flèches du clavier lorsqu'elle est ouverte
+    document.addEventListener('keydown', handleArrowKeyNavigation);
 }
 
 export function closeModal() {
-    toggleModal(false);
+    toggleModal(false); // Masque la modale
+
+    // Retire le gestionnaire de la navigation avec les flèches du clavier lorsqu'elle est fermée
+    document.removeEventListener('keydown', handleArrowKeyNavigation);
 }
 
 function toggleModal(show) {
+    // Sélectionne le conteneur de la modale
     const modal = document.getElementById("contact_modal");
+    // Sélectionne l'encart du photographe pour le masquer lors de l'affichage de la modale
+    const photographerInsert = document.querySelector('.photographer_insert');
+
     if (modal) {
+        // Change la visibilité de la modale
         modal.style.display = show ? "block" : "none";
+        // Active ou désactive le défilement du body en fonction de la visibilité de la modale
         document.body.classList.toggle('no-scroll', show);
+
+        if (show) {
+            // Assure que le focus reste dans la modale
+            trapFocus(modal);
+            // Ajoute un gestionnaire pour fermer la modale avec la touche Échap
+            document.addEventListener('keydown', handleEscKey);
+        } else {
+            // Retire le gestionnaire pour fermer la modale avec la touche Échap
+            document.removeEventListener('keydown', handleEscKey);
+        }
+    }
+    
+    if (photographerInsert) {
+        // Masque ou affiche l'encart du photographe
+        photographerInsert.classList.toggle('hidden', show);
+    }
+}
+
+/**
+ * Gère la fermeture de la modale lorsque la touche Échap est pressée
+ * @param {KeyboardEvent} event - L'événement de touche qui déclenche la fermeture de la modale avec la touche Échap
+ */
+function handleEscKey(event) {
+    if (event.key === 'Escape') {
+        closeModal(); // Ferme la modale
+    }
+}
+
+/**
+ * Gère la navigation avec les flèches du clavier dans la modale
+ * @param {KeyboardEvent} event - L'événement de touche qui déclenche la navigation avec les flèches du clavier
+ */
+function handleArrowKeyNavigation(event) {
+    // Sélectionne tous les éléments focusables dans la modale
+    const focusableElements = document.querySelectorAll('#contact_modal .text-control, #contact_modal textarea, #contact_modal button');
+    const activeElementIndex = Array.from(focusableElements).indexOf(document.activeElement);
+
+    if (event.key === 'ArrowDown') {
+        event.preventDefault(); // Empêche le comportement par défaut
+        if (activeElementIndex < focusableElements.length - 1) {
+            // Passe au prochain élément focusable
+            focusableElements[activeElementIndex + 1].focus();
+        }
+    } else if (event.key === 'ArrowUp') {
+        event.preventDefault(); // Empêche le comportement par défaut
+        if (activeElementIndex > 0) {
+            // Passe à l'élément focusable précédent
+            focusableElements[activeElementIndex - 1].focus();
+        }
+    }
+}
+
+/**
+ * Gère la navigation avec la touche Tab dans la modale
+ * @param {KeyboardEvent} e - L'événement de clavier qui déclenche la gestion de la touche Tab
+ */
+function trapFocus(element) {
+    // Sélectionne tous les éléments focusables dans la modale
+    const focusableElements = element.querySelectorAll(
+        'button, input, textarea, [tabindex]:not([tabindex="-1"])'
+    );
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+    // Ajoute un gestionnaire d'événements pour capturer la touche Tab
+    element.addEventListener('keydown', (e) => {
+        const isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+        if (!isTabPressed) {
+            return;
+        }
+
+        if (e.shiftKey) {
+            // Shift + Tab
+            if (document.activeElement === firstFocusableElement) {
+                e.preventDefault(); // Empêche le comportement par défaut
+                lastFocusableElement.focus(); // Passe au dernier élément focusable
+            }
+        } else {
+            // Tab
+            if (document.activeElement === lastFocusableElement) {
+                e.preventDefault(); // Empêche le comportement par défaut
+                firstFocusableElement.focus(); // Passe au premier élément focusable
+            }
+        }
+    });
+
+    // Initialise le focus sur le premier élément focusable
+    if (focusableElements.length) {
+        firstFocusableElement.focus();
     }
 }
 
@@ -83,12 +227,13 @@ function validateField(fieldId) {
     const field = document.getElementById(fieldId);
     if (!field) return false;
 
-    // Utiliser trim() pour les champs 'first' et 'last'
+    // Utilise trim() pour les champs 'first' et 'last' afin d'éliminer les espaces superflus
     let value = field.value;
     if (fieldId === 'first' || fieldId === 'last') {
         value = value.trim();
     }
 
+    // Définit les fonctions de validation pour chaque champ
     const validators = {
         'first': value => /^[a-zA-Z]{2,}$/.test(value),
         'last': value => /^[a-zA-Z]{2,}$/.test(value),
@@ -96,6 +241,7 @@ function validateField(fieldId) {
         'message': value => value.trim() !== ""
     };
 
+    // Messages d'erreur pour chaque champ
     const errorMessages = {
         'first': "!! Le champ doit contenir au moins 2 lettres / Les chiffres ne sont pas autorisés !!",
         'last': "!! Le champ doit contenir au moins 2 lettres / Les chiffres ne sont pas autorisés !!",
@@ -103,17 +249,20 @@ function validateField(fieldId) {
         'message': "!! Le champ du message ne doit pas être vide !!"
     };
 
+    // Valide le champ et récupère le message d'erreur correspondant
     const condition = validators[fieldId](value);
     const errorMessage = errorMessages[fieldId];
     const errorSpan = field.nextElementSibling;
 
     if (!condition) {
+        // Affiche le message d'erreur si la validation échoue
         field.setAttribute('data-error-visible', 'true');
         if (errorSpan) {
             errorSpan.textContent = errorMessage;
             errorSpan.setAttribute('data-error-visible', 'true');
         }
     } else {
+        // Masque le message d'erreur si la validation réussit
         field.removeAttribute('data-error-visible');
         if (errorSpan) {
             errorSpan.textContent = '';
@@ -128,17 +277,32 @@ function logFieldValue(fieldId) {
     const field = document.getElementById(fieldId);
     if (!field) return;
 
+    // Utilise trim() pour les champs 'first' et 'last' afin d'éliminer les espaces superflus
     let value = field.value;
     if (fieldId === 'first' || fieldId === 'last') {
         value = value.trim();
     }
 
-    // Affiche les valeurs des champs 'first', 'last' et 'email' dans la console lorsque l'utilisateur a fini de remplir le champ
+    // Affiche les valeurs des champs 'first', 'last' et 'email' dans la console
     if (fieldId === 'first' || fieldId === 'last' || fieldId === 'email') {
         console.log(`${fieldId}: ${value}`);
     }
 }
 
+function resetForm() {
+    // Sélectionne le formulaire
+    const form = document.querySelector("form");
+    if (form) {
+        form.reset(); // Réinitialise les champs du formulaire
+        // Réinitialise les messages d'erreur
+        document.querySelectorAll('.error-message').forEach(span => {
+            span.textContent = '';
+            span.setAttribute('data-error-visible', 'false');
+        });
+    }
+}
+
 export function validateForm() {
+    // Valide chaque champ et retourne vrai si tous les champs sont valides
     return ['first', 'last', 'email', 'message'].every(validateField);
 }
